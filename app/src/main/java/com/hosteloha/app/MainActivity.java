9b432336f7941281cd1b3f7b377a5b4c1fee7647
suhaas-livcd <suhaas-livcd@users.noreply.github.com>
2019-09-30 23:32:03 +0530

    Changed package name

--- app/src/main/java/com/hosteloha/app/MainActivity.java
+++ app/src/main/java/com/hosteloha/app/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.idlikadai.hosteloha;
+package com.hosteloha.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.hosteloha.R;
 
 public class MainActivity extends AppCompatActivity {
 
