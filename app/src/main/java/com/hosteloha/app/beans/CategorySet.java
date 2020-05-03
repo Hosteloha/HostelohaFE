261da51687729c4bc3494fc9b8936597e8ba558f
Vinay Polimera <vinaykumarp987@gmail.com>
2020-05-03 17:29:03 +0530

    Added changes to parse the updated Product Object
    Signed-off-by: srinivas013 <srinivaspolimera009@gmail.com>

---
+++ app/src/main/java/com/hosteloha/app/beans/CategorySet.java
@@ -0,0 +1,51 @@
+package com.hosteloha.app.beans;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CategorySet {
+
+    @SerializedName("category_name")
+    private String mainCategory = "Beauty";
+    @SerializedName("sub_category1")
+    private String subCategory1 = "FaceCreams";
+    @SerializedName("sub_category2")
+    private String subCategory2 = "Facewash";
+
+    public CategorySet(String mainCategory, String subCategory1, String subCategory2) {
+        this.mainCategory = mainCategory;
+        this.subCategory1 = subCategory1;
+        this.subCategory2 = subCategory2;
+    }
+
+    public String getMainCategory() {
+        return mainCategory;
+    }
+
+    public void setMainCategory(String mainCategory) {
+        this.mainCategory = mainCategory;
+    }
+
+    public String getSubCategory1() {
+        return subCategory1;
+    }
+
+    public void setSubCategory1(String subCategory1) {
+        this.subCategory1 = subCategory1;
+    }
+
+    public String getSubCategory2() {
+        return subCategory2;
+    }
+
+    public void setSubCategory2(String subCategory2) {
+        this.subCategory2 = subCategory2;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "[ CategorySet == mainCategory = " + mainCategory + ", subCategory1 = " + subCategory1 + ", subCategory2 = " + subCategory2 + " ]";
+    }
+}
